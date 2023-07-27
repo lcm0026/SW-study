9e047d1575f7855d314bdfdf934bec8109adb4c2
lcm0026 <139477744+lcm0026@users.noreply.github.com>
2023-07-27 14:33:55 +0900

    1978

--- CarefreeBitterImplementation (2)/main.c
+++ CarefreeBitterImplementation (2)/main.c
@@ -1,39 +1,23 @@
 #include<stdio.h>
-#include <string.h>
 int main(void)
 {
-    int i, j, max, result=0, len;
-    char arr[1000000];
-    int cnt[26] = {0, };
-    int a = 0;
-    scanf("%s", arr);
-    len = strlen(arr);
-    for(i = 'a'; i <= 'z'; i++){
-        for(j = 0; j < len; j++){
-           if(i == arr[j])
-               cnt[i-'a']++;
+    int n;
+    int num, count = 0;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        for (int j = 2; j <= num; j++)
+        {
+            if(num == j)
+                count++;
+            if (num % j == 0)
+                break;
         }
     }
-    for(i = 'A'; i <= 'Z'; i++){
-        for(j = 0; j < len; j++){
-           if(i == arr[j])
-               cnt[i-'A']++;
-        }
-    }
-    max = cnt[0];
-    for(i = 1; i < 26; i++){
-        if(max < cnt[i]){
-            max = cnt[i];
-            a = i;
-        }
-    }
-    for(i = 0; i < 26; i++){
-        if(max == cnt[i])
-            result++;
-    }
-    if(result > 1)
-        printf("?\n");
-    else
-        printf("%c", a+'A');
+
+    printf("%d", count);
+
     return 0;
 }
